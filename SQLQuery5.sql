87fb470766dec5409ab467e26a263a47f70384b0
namnguyen55p24 <115022325+namnguyen55p24@users.noreply.github.com>
2023-04-04 23:06:18 +0700

    Add files via upload

---
+++ SQLQuery5.sql
@@ -0,0 +1,80 @@
+﻿-- câu 1
+CREATE FUNCTION dbo.showSan_pham()
+RETURNS TABLE
+AS
+RETURN
+SELECT *
+FROM Sanpham;
+-- câu 2
+CREATE FUNCTION TinhTongGiaTriNhap(@namx INT, @namy INT)
+RETURNS MONEY
+AS
+BEGIN
+    DECLARE @tongGiaTriNhap MONEY
+    SELECT @tongGiaTriNhap = SUM(dongiaN * soluongN)
+    FROM Nhap
+    WHERE YEAR(ngaynhap) >= @namx AND YEAR(ngaynhap) <= @namy
+    RETURN @tongGiaTriNhap
+END
+-- câu 3
+CREATE FUNCTION ThongKeSoLuongNhapXuat(@tenSP NVARCHAR(50), @nam INT)
+RETURNS INT
+AS
+BEGIN
+    DECLARE @soLuongNhapXuat INT
+
+    SELECT @soLuongNhapXuat = SUM(COALESCE(n.SoluongN, 0) - COALESCE(x.SoluongX, 0))
+    FROM SanPham sp
+    LEFT JOIN Nhap n ON sp.MaSP = n.MaSP
+    LEFT JOIN Xuat x ON sp.MaSP = x.MaSP AND YEAR(x.NgayXuat) = @nam
+    WHERE sp.TenSP = @tenSP AND YEAR(n.NgayNhap) = @nam
+
+    RETURN @soLuongNhapXuat
+END
+-- câu 4
+CREATE FUNCTION TinhTongGiaTriNhapNgay(@ngayX DATE, @ngayY DATE)
+RETURNS MONEY
+AS
+BEGIN
+    DECLARE @tongGiaTriNhap MONEY
+
+    SELECT @tongGiaTriNhap = SUM(dongiaN * soluongN)
+    FROM Nhap
+    WHERE ngaynhap >= @ngayX AND ngaynhap <= @ngayY
+
+    RETURN @tongGiaTriNhap
+END
+-- câu 5
+CREATE FUNCTION fn_TongGiaTriXuat(@tenHang NVARCHAR(20), @nam INT)
+RETURNS MONEY
+AS
+BEGIN
+  DECLARE @tongGiaTriXuat MONEY;
+  SELECT @tongGiaTriXuat = SUM(S.giaban * X.soluongX)
+  FROM Xuat X
+  JOIN Sanpham S ON X.masp = S.masp
+  JOIN Hangsx H ON S.mahangsx = H.mahangsx
+  WHERE H.tenhang = @tenHang AND YEAR(X.ngayxuat) = @nam;
+  RETURN @tongGiaTriXuat;
+END;
+
+--câu 6
+CREATE FUNCTION fn_ThongKeNhanVienTheoPhong (@tenPhong NVARCHAR(30))
+RETURNS TABLE
+AS
+RETURN
+    SELECT phong, COUNT(manv) AS soLuongNhanVien
+    FROM Nhanvien
+    WHERE phong = @tenPhong
+    GROUP BY phong;
+-- câu 7
+CREATE FUNCTION sp_xuat_trong_ngay(@ten_sp NVARCHAR(20), @ngay_xuat DATE)
+RETURNS INT
+AS
+BEGIN
+  DECLARE @so_luong_xuat INT
+  SELECT @so_luong_xuat = SUM(soluongX)
+  FROM Xuat x JOIN Sanpham sp ON x.masp = sp.masp
+  WHERE sp.tensp = @ten_sp AND x.ngayxuat = @ngay_xuat
+  RETURN @so_luong_xuat
+END
